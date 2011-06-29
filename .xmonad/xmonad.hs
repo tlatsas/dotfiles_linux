293b6f49160b455070666a2b69c46611958e8a9c
Tasos Latsas <tlatsas2000@gmail.com>
2011-06-29 20:22:33 +0300

    add custom xmonad layouts

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,6 +1,4 @@
 import XMonad
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
@@ -9,12 +7,43 @@ import System.Exit
 import qualified XMonad.StackSet    as W
 import qualified Data.Map           as M
 
+-- hooks
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.UrgencyHook
+import XMonad.Hooks.ManageHelpers
+
+-- xmonad prompt
+import XMonad.Util.Run(spawnPipe)
+import qualified XMonad.Prompt as P
+import XMonad.Prompt.Shell
+import XMonad.Prompt
+import XMonad.Prompt.AppendFile (appendFilePrompt)
+import XMonad.Prompt.RunOrRaise
+
+-- layouts
+import XMonad.Layout.NoBorders
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.Reflect
+import XMonad.Layout.IM
+import XMonad.Layout.Tabbed
+import XMonad.Layout.PerWorkspace (onWorkspace)
+import XMonad.Layout.Grid
+import Control.OldException(catchDyn,try)
+import XMonad.Layout.ComboP
+import XMonad.Layout.Column
+import XMonad.Layout.Named
+import XMonad.Layout.TwoPane
+import Data.Ratio ((%))
+import Data.List (isInfixOf)
+
 
 main = do
     spawn "sh ${HOME}/.xmonad/autostart.sh"
     xmproc <- spawnPipe "xmobar"
-    xmonad $ defaultConfig { manageHook = manageDocks <+> manageHook defaultConfig
-            , layoutHook = avoidStruts  $  layoutHook defaultConfig
+    xmonad $ withUrgencyHook NoUrgencyHook $ defaultConfig { manageHook = myManageHook <+> manageDocks
+            --, layoutHook = avoidStruts  $  layoutHook defaultConfig
+            , layoutHook = myLayoutHook
             , logHook = dynamicLogWithPP xmobarPP { ppOutput = hPutStrLn xmproc, ppTitle = xmobarColor "green" "" . shorten 60 }
             , terminal = myTerminal
             , workspaces = myWorkspaces
@@ -27,7 +56,7 @@ main = do
 
 -- workspaces
 myWorkspaces :: [WorkspaceId]
-myWorkspaces = ["1:web", "2:code", "3:chat", "4:mail", "5:media", "6:docs", "7:dl", "8:games", "9" ]
+myWorkspaces = ["1:web", "2:code", "3:chat", "4:mail", "5:media", "6:docs", "7:dl", "8:games", "9:other" ]
 
 -- terminal
 myTerminal :: String
@@ -46,8 +75,55 @@ myNormalBorderColor, myFocusedBorderColor :: String
 myNormalBorderColor = "#141414"
 myFocusedBorderColor = "#e0e0e0"
 
--- layouts
--- TODO
+-- hooks --
+-- switch apps to workspace
+myManageHook :: ManageHook
+myManageHook = composeAll . concat $
+                [[isFullscreen                      --> doFullFloat
+                , className =? "Xmessage"           --> doCenterFloat
+                , className =? "Gimp"               --> doShift "9:other"
+                , className =? "Pidgin"             --> doShift "3:chat"
+                , className =? "Skype"              --> doShift "3:chat"
+                , className =? "Thunderbird"        --> doShift "4:mail"
+                , className =? "MPlayer"            --> doShift "5:media"
+                , className =? "SMPlayer"           --> doShift "5:media"
+                , className =? "Wine"               --> doShift "8:games"
+                , title     =? "Minecraft Launcher" --> doShift "8:games"
+                , title     =? "Minecraft Launcher" --> doFloat
+                , fmap ("libreoffice" `isInfixOf`) className --> doShift "5:doc"
+                , className =? "MPlayer"            --> (ask >>= doF . W.sink)
+                ]]
+
+-- layout
+myLayoutHook = onWorkspace "3:chat" imLayout $ onWorkspace "4:mail" webL $ onWorkspace "5:media" fullL $ onWorkspace "9:other" gimpLayout $ standardLayouts
+    where
+        standardLayouts = avoidStruts $ (tiled ||| reflectTiled ||| Mirror tiled ||| Grid ||| Full ||| simpleTabbed)
+
+        --Layouts
+        tiled = smartBorders (ResizableTall 1 (2/100) (1/2) [])
+        reflectTiled = (reflectHoriz tiled)
+        full = noBorders Full
+
+        --Im Layout
+        imLayout = avoidStruts $ smartBorders $ withIM ratio pidginRoster $ reflectHoriz $ withIM skypeRatio skypeRoster (tiled ||| reflectTiled ||| Grid) where
+                chatLayout = Grid
+                ratio = (1%9)
+                skypeRatio = (1%8)
+                pidginRoster = And (ClassName "Pidgin") (Role "buddy_list")
+                skypeRoster = (ClassName "Skype") `And`
+                               (Not (Title "Options")) `And`
+                               (Not (Role "Chats")) `And`
+                               (Not (Role "CallWindowForm"))
+
+        webL = avoidStruts $ full ||| simpleTabbed ||| tiled ||| reflectHoriz tiled
+
+        gimpLayout = withIM (0.11) (Role "gimp-toolbox") $
+                reflectHoriz $
+                withIM (0.15) (Role "gimp-dock") Full
+
+        --VirtualLayout
+        fullL = avoidStruts $ full
+
 
 -- keybindings
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
