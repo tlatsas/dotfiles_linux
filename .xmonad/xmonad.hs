423117fd2213cca39a59bf4712d63b03f0a143eb
Tasos Latsas <tlatsas2000@gmail.com>
2011-07-13 18:51:37 +0300

    add kbindings to lock screen

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -219,6 +219,9 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     -- Deincrement the number of windows in the master area
     , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
 
+    -- Lock screen
+    , ((modm .|. shiftMask, xK_l     ), spawn "xscreensaver-command -lock")
+
     -- Toggle the status bar gap
     -- Use this binding with avoidStruts from Hooks.ManageDocks.
     -- See also the statusBar function from Hooks.DynamicLog.
