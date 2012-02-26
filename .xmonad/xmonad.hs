59ea177b530790f6ac75efd9db542e7d50a6896f
Tasos Latsas <tlatsas2000@gmail.com>
2012-02-26 13:00:33 +0200

    move xmobar config inside xmonad folder

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -41,7 +41,7 @@ import XMonad.Hooks.SetWMName
 
 
 main = do
-    xmproc <- spawnPipe "xmobar"
+    xmproc <- spawnPipe "xmobar ~/.xmonad/xmobarrc"
     xmonad $ withUrgencyHook NoUrgencyHook $ defaultConfig { manageHook = myManageHook <+> manageDocks
             , layoutHook = myLayoutHook
             , logHook = myLogHook xmproc
