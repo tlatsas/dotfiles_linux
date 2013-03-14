473674bf1ce21d938810c28e1ddefa999f1cbd7f
Tasos Latsas <tlatsas2000@gmail.com>
2013-03-14 17:21:34 +0200

    do not turn off laptop screen

--- bin/display-out.sh
+++ bin/display-out.sh
@@ -5,7 +5,7 @@
 
 if lsmod | grep -q 'nouveau'; then
     if xrandr | grep -q 'VGA-1 connected'; then
-        xrandr --output LVDS-1 --off --output VGA-1 --mode 1680x1050 --rate 60 --dpi 90
+        xrandr --output VGA-1 --mode 1680x1050 --rate 60 --dpi 90
     else
         # laptop screen - only adjust DPI
         xrandr --dpi 100
@@ -14,7 +14,7 @@ else
     # nvidia propriatary
     if xrandr | grep -q 'VGA-0 connected'; then
         # nvidia driver should pick the right resolution/rate for us
-        xrandr --output LVDS-0 --off --output VGA-0 --dpi 90
+        xrandr --output VGA-0 --dpi 90
     else
         # laptop screen - only adjust DPI
         xrandr --dpi 100
